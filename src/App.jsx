78052213c41b29645ab4fc7469a140db55e09508
Aqsavalli <aqsavalli@gmail.com>
2025-10-15 11:00:32 +0530

    15th oct Taskform

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Taskform from "./Components/Taskform";
+import TaskList from "./Components/TaskList";
+import Progresstracker from "./Components/Progresstracker";
+
+function App() {
+  return (
+    <div>
+      <h1>Task Nexus</h1>
+      <p>Our friendly Task Manager </p>
+      <Taskform />
+      <TaskList />
+      <Progresstracker />
+      <button>Clear All Tasks</button>
+    </div>
+  )
+}
+
+export default App;
